b9b056e5de887434d46d59ecae13b595098bc511
Emad <emad.ibrahim533@gmail.com>
2020-07-06 10:54:27 +0200

    sidekick

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var test = document.header.getElementById(function (){
+    test.document.getElementById("Controls").style.backGroundColor = "red"
+})
+
+console.log(document.getElementById("Controls"));
+
+
+var firstC = document.getElementsByClassName('container-fluid1')
+firstC.style.color = "red";
+console.log(firstC)
